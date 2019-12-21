2423d6aeda51b4b3d8c7fc450089f081a0cea601
Rachel Lew <rlew07@gmail.com>
2019-12-21 15:06:18 -0700

    Change cancele to cancel in route

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
-  patch "/orders/:id", to: "orders#cancele"
+  patch "/orders/:id", to: "orders#cancel"
 
   get "/register", to: "users#new"
   post "/users", to: "users#create"
