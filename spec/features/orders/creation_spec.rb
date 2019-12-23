214328b3484957c3394ef3082c17b35e527048c2
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-23 10:57:57 -0800

    Update order_controller index to only index current_user's orders. Edit order index page to display sections and info order creation spec was looking for

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -14,8 +14,7 @@ RSpec.describe("Order Creation") do
       @meg = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
-      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 200, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
       visit "/items/#{@paper.id}"
       click_on "Add To Cart"
       visit "/items/#{@paper.id}"
@@ -29,7 +28,11 @@ RSpec.describe("Order Creation") do
       click_on "Checkout"
     end
 
-    xit 'I can create a new order' do
+    it 'I can create a new order' do
+      user = User.create!(name: "User", address: "1230 East Street", city: "Boulder", state: "CO", zip: 98273, email: "user@user.com", password: "user", password_confirmation: "user")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       name = "Bert"
       address = "123 Sesame St."
       city = "NYC"
@@ -59,37 +62,41 @@ RSpec.describe("Order Creation") do
       within "#item-#{@paper.id}" do
         expect(page).to have_link(@paper.name)
         expect(page).to have_link("#{@paper.merchant.name}")
-        expect(page).to have_content("$#{@paper.price}")
+        expect(page).to have_content("$#{@paper.price/100.0}")
         expect(page).to have_content("2")
-        expect(page).to have_content("$40")
+        expect(page).to have_content("$0.40")
       end
 
       within "#item-#{@tire.id}" do
         expect(page).to have_link(@tire.name)
         expect(page).to have_link("#{@tire.merchant.name}")
-        expect(page).to have_content("$#{@tire.price}")
+        expect(page).to have_content("$#{@tire.price/100.0}")
         expect(page).to have_content("1")
-        expect(page).to have_content("$100")
+        expect(page).to have_content("$1.00")
       end
 
       within "#item-#{@pencil.id}" do
         expect(page).to have_link(@pencil.name)
         expect(page).to have_link("#{@pencil.merchant.name}")
-        expect(page).to have_content("$#{@pencil.price}")
+        expect(page).to have_content("$#{@pencil.price/100}")
         expect(page).to have_content("1")
-        expect(page).to have_content("$2")
+        expect(page).to have_content("$2.00")
       end
 
       within "#grandtotal" do
-        expect(page).to have_content("Total: $142")
+        expect(page).to have_content("Total: $3.40")
       end
 
       within "#datecreated" do
-        expect(page).to have_content(new_order.created_at)
+        expect(page).to have_content(new_order.created_at.strftime('%m/%d/%y'))
       end
     end
 
-    xit 'i cant create order if info not filled out' do
+    it 'i cant create order if info not filled out' do
+      user = User.create!(name: "User", address: "1230 East Street", city: "Boulder", state: "CO", zip: 98273, email: "user@user.com", password: "user", password_confirmation: "user")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       name = ""
       address = "123 Sesame St."
       city = "NYC"
@@ -104,7 +111,7 @@ RSpec.describe("Order Creation") do
 
       click_button "Create Order"
 
-      expect(page).to have_content("Please complete address form to create an order.")
+      expect(page).to have_content("Name can't be blank")
       expect(page).to have_button("Create Order")
     end
 
@@ -128,7 +135,7 @@ RSpec.describe("Order Creation") do
       click_button "Create Order"
 
       new_order = Order.last
-      
+
       expect(current_path).to eq("/profile/orders")
       expect(page).to have_content("Your order was created!")
       expect(page).to have_content("Order Number: #{new_order.id}")
