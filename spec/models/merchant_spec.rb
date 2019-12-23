759db9635d066aae692bdca2a1856ecb484a1350
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-23 08:27:46 -0800

    Add test for order_price method for merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -73,16 +73,37 @@ describe Merchant, type: :model do
       item_order_3 = order_3.item_orders.create!(item: skis, price: skis.price, quantity: 2)
 
       bike_stats = bike_shop.orders_info
-      require "pry"; binding.pry
+
       expect(bike_stats[0].order_id).to eq(order_1.id)
       expect(bike_stats[1].order_id).to eq(order_2.id)
       expect(bike_stats[0].quantity).to eq(6)
       expect(bike_stats[1].quantity).to eq(20)
-      #currently not working
-      
-      expect(bike_stats[0].price).to eq(550)
-      expect(bike_stats[1].price).to eq(2000)
     end
 
+    it "#order_price" do
+      bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      ski_shop = Merchant.create(name: "Ski Palace", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+      tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      skis = ski_shop.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+
+      user = User.create!(name: "User", address: "1230 East Street", city: "Boulder", state: "CO", zip: 98273, email: "user@user.com", password: "user", password_confirmation: "user")
+
+      order_1 = user.orders.create!(name: 'Bob', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_2 = user.orders.create!(name: 'Bob', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_3 = user.orders.create!(name: 'Bob', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      item_order_1 = order_1.item_orders.create!(item: chain, price: chain.price, quantity: 1)
+      item_order_1 = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 5)
+      item_order_2 = order_2.item_orders.create!(item: tire, price: tire.price, quantity: 20)
+      item_order_2 = order_2.item_orders.create!(item: skis, price: skis.price, quantity: 7)
+      item_order_3 = order_3.item_orders.create!(item: skis, price: skis.price, quantity: 2)
+
+      expect(bike_shop.order_price(order_1.id)).to eq(550)
+      expect(bike_shop.order_price(order_2.id)).to eq(2000)
+      expect(bike_shop.order_price(order_3.id)).to eq(0)
+      expect(ski_shop.order_price(order_2.id)).to eq(1260)
+      expect(ski_shop.order_price(order_3.id)).to eq(360)
+    end
   end
 end
