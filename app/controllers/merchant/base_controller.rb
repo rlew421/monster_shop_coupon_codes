259925112a8fbca7be30389233789ea6381e35bb
Rachel Lew <rlew07@gmail.com>
2019-12-23 13:06:11 -0700

    Change before_action so that an admin can access a merchant's items index

--- app/controllers/merchant/base_controller.rb
+++ app/controllers/merchant/base_controller.rb
@@ -1,7 +1,12 @@
 class Merchant::BaseController < ApplicationController
-  before_action :require_merchant
+  before_action :require_merchant, only: [:show]
+  before_action :require_admin_or_merchant, only: [:index]
 
   def require_merchant
     render file: '/public/404' unless current_user && (current_user.merchant_admin? || current_user.merchant_employee?)
+  end
+
+  def require_admin_or_merchant
+    render file: '/public/404' unless current_user && (current_user.merchant_admin? || current_user.merchant_employee? || current_user.admin?)
   end
 end
