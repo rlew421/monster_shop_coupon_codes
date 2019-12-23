d29225b7e7b8d57ce31af1fe17802a729ec49eda
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-22 23:27:38 -0700

    Create orders_info method...working to gather order quantity and order id

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -30,12 +30,24 @@ class Merchant <ApplicationRecord
     #each order
     #date
     #total quantity of items
-
     #total value
-    
-    item_orders.select('order_id, SUM(item_orders.quantity) AS total_quantity, SUM(item_orders.price) AS total_price').group(:order_id)
+
+
+    # can I query where on multiple params?
+    # ItemOrder.select('order_id, orders.created_at, SUM(item_orders.quantity) AS total_quantity, SUM(item_orders.price) AS total_price').joins(:orders).where("item_orders.id = #{ids}").group('item_orders.order_id')
+
+
+    item_orders.select('order_id, SUM(item_orders.quantity) AS quantity, SUM(item_orders.price) AS price').group(:order_id)
+
 
     # SELECT orders.created_at, SUM(item_orders.quantity) AS total_quantity, SUM(item_orders.price) AS total_price FROM item_orders JOIN orders ON item_orders.order_id = orders.id GROUP BY orders.id;
+  end
+
+  def order_price(order_id)
+    orders = item_orders.where(order_id: order_id)
+      orders.sum do |order|
+        order.subtotal
+      end
   end
 
 end
