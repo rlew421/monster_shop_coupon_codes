a6e3cf0fea21ed3b829b7e92994be91562913aee
Rachel Lew <rlew07@gmail.com>
2020-01-06 13:59:11 -0700

    Add index and update methods
    Co-Authored-By: LainMcGrath <53873820+LainMcGrath@users.noreply.github.com>
    Co-Authored-By: Dylan C <54859243+dylanconnolly@users.noreply.github.com>
    Co-Authored-By: Santiago Caraveo <Santiag0C@users.noreply.github.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,5 +1,20 @@
 class Admin::MerchantsController < Admin::BaseController
+  def index
+    @merchants = Merchant.all
+  end
+
   def show
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @merchant.toggle(:enabled?).save
+    if @merchant.enabled?
+      flash[:notice] = "#{@merchant.name} has been enabled."
+    else
+      flash[:notice] = "#{@merchant.name} has been disabled."
+    end
+    redirect_to '/admin/merchants'
   end
 end
