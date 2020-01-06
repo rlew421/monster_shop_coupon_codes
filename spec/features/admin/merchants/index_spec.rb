271f8f0171c83e5d1ea92fac9457c03a8962b024
Rachel Lew <rlew07@gmail.com>
2020-01-06 14:44:37 -0700

    Add test that when admin disables a merchants, that merchant's items are deactivated
    Co-Authored-By: LainMcGrath <53873820+LainMcGrath@users.noreply.github.com>
    Co-Authored-By: Santiago Caraveo <Santiag0C@users.noreply.github.com>
    Co-Authored-By: Dylan C <54859243+dylanconnolly@users.noreply.github.com>

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -60,5 +60,54 @@ RSpec.describe 'when I visit the merchant index page' do
     end
 
     expect(page).to have_content("#{dog_shop.name} has been disabled")
+
+    within "#merchant-#{dog_shop.id}" do
+      click_button "Enable"
+    end
+
+    expect(current_path).to eq('/admin/merchants')
+
+    within "#merchant-#{dog_shop.id}" do
+      expect(page).to have_button "Disable"
+      expect(page).to_not have_button "Enable"
+    end
+    expect(page).to have_content("#{dog_shop.name} has been enabled")
+  end
+
+  it "when I disable an enabled merchant, all of that merchant's items are deactivated" do
+    admin = User.create!(name: "Admin", address: "1230 East Street", city: "Boulder", state: "CO", zip: 98273, email: "admin@admin.com", password: "admin", password_confirmation: "admin", role: 3)
+    bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+    pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit '/admin/merchants'
+
+    within "#merchant-#{dog_shop.id}" do
+      click_button "Disable"
+    end
+
+    dog_shop.reload
+    pull_toy.reload
+    dog_bone.reload
+
+    expect(pull_toy.active?).to eq(false)
+    expect(dog_bone.active?).to eq(false)
+
+    visit '/admin/merchants'
+
+    within "#merchant-#{dog_shop.id}" do
+      click_button "Enable"
+    end
+
+    dog_shop.reload
+    pull_toy.reload
+    dog_bone.reload
+
+    expect(pull_toy.active?).to eq(true)
+    expect(dog_bone.active?).to eq(true)
   end
 end
