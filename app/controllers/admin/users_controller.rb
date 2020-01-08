c8b59dcf88ad0c3e583f014735c10cb46feb103b
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-08 12:29:45 -0700

    Update tests to no longer assert merchant EE and merchant admin users don't appear on admin user ind ex page

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -39,7 +39,8 @@ class Admin::UsersController < Admin::BaseController
 
   def change_role
     user = User.find(params[:user_id])
-    if request.env['PATH_INFO'] == "/users/#{user.id}/upgrade_to_merchant_employee"
+    require "pry"; binding.pry
+    if request.env['PATH_INFO'] == "admin/users/#{user.id}/upgrade_to_merchant_employee"
       user.update_column(:role, 1)
       redirect_to '/admin/users'
     else request.env['PATH_INFO'] == "/users/#{user.id}/upgrade_to_merchant_admin"
