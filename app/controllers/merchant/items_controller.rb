ff462094dfd074667e9dade816115c22f9ca8389
Santiago Caraveo <santiagocbrvg@hotmail.com>
2020-01-08 16:13:19 -0700

    adds create method and updates item params

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,4 +1,36 @@
 class Merchant::ItemsController < Merchant::BaseController
+
   def index
+    @merchant = Merchant.find(params["merchant_id"])
+  end
+
+  def new
+    @merchant = Merchant.find(params["merchant_id"])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    item = @merchant.items.create(item_params)
+    # binding.pry
+
+      if item.save
+        # binding.pry
+        redirect_to "/merchants/#{@merchant.id}/items"
+      else
+        flash[:error] = item.errors.full_messages.to_sentence
+        # binding.pry
+        # render :new
+        redirect_to "/merchant/#{@merchant.id}/items/new"
+      end
+    end
+
+  def item_params
+    # binding.pry
+    if params["image"] == ""
+      params["image"]= 'https://www.broadwayjiujitsu.com/wp-content/uploads/2017/04/default-image.jpg'
+      params.permit(:name,:description,:price,:inventory,:image)
+    else
+      params.permit(:name,:description,:price,:inventory,:image)
+    end
   end
 end
