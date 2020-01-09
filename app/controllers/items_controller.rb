09f11942ee3a32eacc57ac9412805b60f7d85baa
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-09 13:18:03 -0700

    Update item views and actions to only use @item instance variable and to access merchant in view through item.merchant

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,15 +14,15 @@ class ItemsController<ApplicationController
   end
 
   def new
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = @merchant.items.new
+    merchant = Merchant.find(params[:merchant_id])
+    @item = merchant.items.new
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = @merchant.items.create(item_params)
+    merchant = Merchant.find(params[:merchant_id])
+    @item = merchant.items.create(item_params)
     if @item.save
-      redirect_to "/merchants/#{@merchant.id}/items"
+      redirect_to "/merchants/#{merchant.id}/items"
     else
       flash[:error] = @item.errors.full_messages.to_sentence
       render :new
