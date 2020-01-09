5002c9bb88a755af36287986eb8603ccd248de40
Santiago Caraveo <santiagocbrvg@hotmail.com>
2020-01-09 15:16:27 -0700

    addsitem update and cerate and params method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,9 +14,7 @@ class ItemsController<ApplicationController
   end
 
   def new
-
     @merchant = Merchant.find(params[:merchant_id])
-
   end
 
   def create
@@ -31,14 +29,17 @@ class ItemsController<ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    @item = Item.find_by(params[id: :item_id])
   end
 
+
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
+    merchant = Merchant.find_by(params[:merchant_id])
+    @item = Item.find_by(params[:items_id])
+    @item.update(params_f)
     if @item.save
       redirect_to "/items/#{@item.id}"
+      flash[:succses] = "#{@item.name} has been Updated."
     else
       flash[:error] = @item.errors.full_messages.to_sentence
       render :edit
@@ -54,7 +55,17 @@ class ItemsController<ApplicationController
 
   private
 
+
   def item_params
+    if params["image"] == ""
+      params["image"]= 'https://www.broadwayjiujitsu.com/wp-content/uploads/2017/04/default-image.jpg'
+      params.permit(:name,:description,:price,:inventory,:image)
+    else
+      params.permit(:name,:description,:price,:inventory,:image)
+    end
+  end
+
+  def params_f
     params.permit(:name,:description,:price,:inventory,:image)
   end
 
