70549b8ca40e0494b0c8518244be6448bed1bf17
Santiago Caraveo <santiagocbrvg@hotmail.com>
2020-01-09 15:17:01 -0700

    addsitem update and cerate and params method

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -6,21 +6,41 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def new
     @merchant = Merchant.find(params["merchant_id"])
+    @auto_popu = params_f
   end
 
   def create
     @merchant = Merchant.find(params[:merchant_id])
     item = @merchant.items.create(item_params)
-    # binding.pry
-
+    binding.pry
       if item.save
-        # binding.pry
+        binding.pry
         redirect_to "/merchants/#{@merchant.id}/items"
       else
         flash[:error] = item.errors.full_messages.to_sentence
         # binding.pry
         # render :new
         redirect_to "/merchant/#{@merchant.id}/items/new"
+        @auto_popu = params_f
+      end
+    end
+
+    def edit
+      @item = Item.find(params[:items_id])
+      @merchant = Merchant.find(params[:merchant_id])
+    end
+
+    def update
+      @merchant = Merchant.find(params[:merchant_id])
+      @item = Item.find(params[:items_id])
+      @item.update(params_f)
+      if @item.save
+        redirect_to "/merchant/#{@merchant.id}/items"
+        flash[:succses] = "#{@item} has been Updated."
+      else
+        flash[:error] = @item.errors.full_messages.to_sentence
+        redirect_to "/merchant/#{@merchant.id}/#{@item.id}/edit"
+
       end
     end
 
@@ -32,5 +52,8 @@ class Merchant::ItemsController < Merchant::BaseController
     else
       params.permit(:name,:description,:price,:inventory,:image)
     end
+  end
+  def params_f
+    params.permit(:name,:description,:price,:inventory,:image)
   end
 end
