3fc3aa40cb1d02b1bd018e4b35abffb172d86591
Rachel Lew <rlew07@gmail.com>
2020-01-13 20:21:14 -0700

    Create coupons table

---
+++ db/migrate/20200113202928_create_coupons.rb
@@ -0,0 +1,10 @@
+class CreateCoupons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :coupons do |t|
+      t.string :name
+      t.string :code
+      t.integer :percentage_off
+      t.timestamps
+    end
+  end
+end
