2809482663560102b77d2f67ed6a3667c7195d40
Rachel Lew <rlew07@gmail.com>
2020-01-13 20:20:33 -0700

    Add Coupon spec with validations, test that coupon should belong to merchant and have many orders

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Coupon, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_uniqueness_of :name}
+    it {should validate_presence_of :code}
+    it {should validate_uniqueness_of :code}
+    it {should validate_presence_of :percentage_off}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :merchant}
+    it {should have_many :orders}
+  end
+end
