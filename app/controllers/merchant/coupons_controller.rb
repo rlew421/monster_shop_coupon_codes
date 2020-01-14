379050efad3b0c3adeb5b4c89159a1f065356203
Rachel Lew <rlew07@gmail.com>
2020-01-13 21:20:37 -0700

    Add new and create methods

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -6,4 +6,19 @@ class Merchant::CouponsController < Merchant::BaseController
   def show
     @coupon = Coupon.find(params[:coupon_id])
   end
+
+  def new
+  end
+
+  def create
+    merchant = Merchant.find_by(params[id: :merchant_id])
+    merchant.coupons.create(coupon_params)
+    redirect_to '/merchant/coupons'
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:name, :code, :percentage_off)
+  end
 end
