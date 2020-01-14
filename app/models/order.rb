d3f9342223cdc1f0552dd1a3dccb009fc92b4d7c
Rachel Lew <rlew07@gmail.com>
2020-01-13 20:23:01 -0700

    Validate that order belongs to coupon (optional)

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
   has_many :item_orders
   has_many :items, through: :item_orders
   belongs_to :user
+  belongs_to :coupon, optional: true
 
   validates_presence_of :name,
                         :address,
