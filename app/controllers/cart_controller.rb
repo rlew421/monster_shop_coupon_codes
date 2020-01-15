83911981f9c65bb891cd8538c8a525136fc3a8b3
Rachel Lew <rlew07@gmail.com>
2020-01-14 20:11:29 -0700

    Define apply_coupon method

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -16,6 +16,7 @@ class CartController < ApplicationController
 
   def show
     @items = cart.items
+    @coupon = session[:coupon_id]
   end
 
   def empty
@@ -49,7 +50,12 @@ class CartController < ApplicationController
   # end
 
   def apply_coupon
-
+    coupon = Coupon.find_by(code: params[:code])
+    if coupon
+      session[:coupon_id] = coupon.id
+      flash[:success] = "#{coupon.name} has been applied!"
+    end
+    redirect_to '/cart'
   end
 
   private
