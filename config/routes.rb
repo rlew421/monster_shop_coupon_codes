0b0c1ec58047fc70b9d6ef01888b4a4c37af93e8
Rachel Lew <rlew07@gmail.com>
2020-01-25 16:40:10 -0700

    Remove commented out routes that were replaced by resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,8 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :merchants
-  # get "/merchants", to: "merchants#index"
-  # get "/merchants/new", to: "merchants#new"
-  # get "/merchants/:id", to: "merchants#show"
-  # post "/merchants", to: "merchants#create"
-  # get "/merchants/:id/edit", to: "merchants#edit"
-  # patch "/merchants/:id", to: "merchants#update"
-  # delete "/merchants/:id", to: "merchants#destroy"
 
   resources :items, only: [:index, :show, :edit, :update, :destroy]
-  # get "/items", to: "items#index"
-  # get "/items/:id", to: "items#show"
-  # get "/items/:id/edit", to: "items#edit"
-  # patch "/items/:id", to: "items#update"
 
   resources :merchants do
     resources :items, only: [:index, :new, :create, :edit]
@@ -26,24 +15,12 @@ Rails.application.routes.draw do
     resources :items, only: [:update], to: "merchant/items#update"
     resources :items, only: [:destroy], to: "merchant/items#destroy"
   end
-  # get "/merchants/:merchant_id/items", to: "items#index"
-  # get "/merchants/:merchant_id/items/new", to: "items#new"
-  # post "/merchants/:merchant_id/items", to: "items#create"
-  # get 'merchants/:merchant_id/items/:items_id/edit', to: "items#edit"
-  # patch 'merchants/:merchant_id/items/:item_id', to: "merchant/items#update"
-  # delete 'merchants/:merchant_id/items/:item_id', to: "merchant/items#destroy"
-  # delete "/items/:id", to: "items#destroy"
 
   resources :items do
     resources :reviews, only: [:new, :create]
   end
-  # get "/items/:item_id/reviews/new", to: "reviews#new"
-  # post "/items/:item_id/reviews", to: "reviews#create"
 
   resources :reviews, only: [:edit, :update, :destroy]
-  # get "/reviews/:id/edit", to: "reviews#edit"
-  # patch "/reviews/:id", to: "reviews#update"
-  # delete "/reviews/:id", to: "reviews#destroy"
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
@@ -53,14 +30,10 @@ Rails.application.routes.draw do
   patch '/cart', to: "cart#apply_coupon"
 
   resources :orders, only: [:new, :create, :show]
-  # get "/orders/new", to: "orders#new"
-  # post "/orders", to: "orders#create"
-  # get "/orders/:id", to: "orders#show"
   patch "/orders/:id", to: "orders#cancel"
 
   get "/register", to: "users#new"
   resources :users, only: [:create]
-  # post "/users", to: "users#create"
   get "/users", to: "sessions#show"
   get "/profile", to: "users#show"
   get "/profile/orders", to: "orders#index"
@@ -78,22 +51,9 @@ Rails.application.routes.draw do
     get '/', to: "merchant#show"
     get '/:merchant_id/items', to: "items#index"
     resources :orders, only: [:show]
-    # get '/orders/:id', to: 'orders#show'
     get '/:merchant_id/items/new', to: "items#new"
     post '/:merchant_id/items', to: "items#create"
-    # get '/:merchant_id/:items_id/edit', to: "items#edit"
-    # patch '/:merchant_id/:items_id', to: "items#update"
     get '/orders/:order_id/item_orders/:item_order_id/fulfill', to: 'item_orders#fulfill'
-    # get '/coupons', to: 'coupons#index'
-    # get '/coupons/new', to: 'coupons#new'
-    # get 'coupons/:coupon_id', to: 'coupons#show'
-    # post '/coupons', to: 'coupons#create'
-    # get '/coupons/:coupon_id/edit', to: 'coupons#edit'
-    # patch '/coupons', to: 'coupons#update'
-    # delete '/coupons/:coupon_id', to: 'coupons#destroy'
-  end
-
-  namespace :merchant do
     resources :coupons
   end
 
@@ -109,17 +69,6 @@ Rails.application.routes.draw do
     get '/users/:user_id/upgrade_to_merchant_employee', to: "users#change_role"
     get '/users/:user_id/upgrade_to_merchant_admin', to: "users#change_role"
   end
-
-  # namespace :admin do
-    # get '/', to: "admins#show"
-    # get '/merchants/', to: "merchants#index"
-    # patch '/merchants/:merchant_id', to: 'merchants#update'
-    # get '/merchants/:merchant_id', to: "merchants#show"
-    # get '/users', to: "users#index"
-    # get '/users/:user_id/', to: "users#show"
-    # get '/users/:user_id/profile/edit', to: "users#edit"
-    # patch '/orders/:id', to: "orders#update"
-  # end
 
   scope :admin do
     get '/merchants/:merchant_id/items', to: "merchant/items#index"
